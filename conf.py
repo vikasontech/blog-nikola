696ccf780acae64e0485cfcd8663d438b38697bd
vikas <vikas.on@gmail.com>
2020-01-17 01:31:24 +0700

    fix spelling mistake && incorrect url

--- conf.py
+++ conf.py
@@ -20,7 +20,8 @@ BLOG_AUTHOR = "Vikas Verma"  # (translatable)
 BLOG_TITLE = "Vikas Java Blogs"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://vikasontech.github.io/web-site/"
+SITE_URL = "https://vikasontech.github.io/web/"
+
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://vikasontech.github.io/web-site/"
